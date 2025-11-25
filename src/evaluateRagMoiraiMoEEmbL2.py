3ba7bea4f83eef4c9a8884ef8d468130f434ec39
Juan Pablo Villa Serna <jpvilla1990@gmail.com>
2025-11-25 13:40:56 +0100

    initial commit

---
+++ src/evaluateRagMoiraiMoEEmbL2.py
@@ -0,0 +1,78 @@
+from vectorDB.vectorDBingestion import VectorDBIngestion
+from evaluation.evaluation import Evaluation
+
+vectorDBingestion : VectorDBIngestion = VectorDBIngestion()
+evaluation : Evaluation = Evaluation()
+
+contextLength : int = 32
+predictionLength : int = 16
+
+vectorDBingestion.ingestDatasetsMoiraiMoE(f"domainDatasetEmbL2_{contextLength}_{predictionLength}", False)
+
+loadPretrainedModel : bool = False
+
+report : dict = evaluation.evaluateMoiraiMoERagCA(
+    contextLength=contextLength,
+    predictionLength=predictionLength,
+    numberSamples=100,
+    dataset="ET",
+    collection=f"domainDatasetEmbL2_{contextLength}_{predictionLength}",
+    loadPretrainedRagCA=loadPretrainedModel,
+    fineTunedModel="MoiraiMoE-finetune-ET-epoch=00-step=000200.ckpt",
+    inputSpace=False,
+)
+
+report : dict = evaluation.evaluateMoiraiMoERagCA(
+    contextLength=contextLength,
+    predictionLength=predictionLength,
+    numberSamples=100,
+    dataset="huaweiCloud",
+    collection=f"domainDatasetEmbL2_{contextLength}_{predictionLength}",
+    loadPretrainedRagCA=loadPretrainedModel,
+    fineTunedModel="MoiraiMoE-finetune-huaweiCloud-epoch=00-step=001000.ckpt",
+    inputSpace=False,
+)
+
+report : dict = evaluation.evaluateMoiraiMoERagCA(
+    contextLength=contextLength,
+    predictionLength=predictionLength,
+    numberSamples=100,
+    dataset="power",
+    collection=f"domainDatasetEmbL2_{contextLength}_{predictionLength}",
+    loadPretrainedRagCA=loadPretrainedModel,
+    fineTunedModel="MoiraiMoE-finetune-power-epoch=00-step=000010.ckpt",
+    inputSpace=False,
+)
+
+report : dict = evaluation.evaluateMoiraiMoERagCA(
+    contextLength=contextLength,
+    predictionLength=predictionLength,
+    numberSamples=100,
+    dataset="traffic",
+    collection=f"domainDatasetEmbL2_{contextLength}_{predictionLength}",
+    loadPretrainedRagCA=loadPretrainedModel,
+    fineTunedModel="MoiraiMoE-finetune-traffic-epoch=00-step=000008.ckpt",
+    inputSpace=False,
+)
+
+report : dict = evaluation.evaluateMoiraiMoERagCA(
+    contextLength=contextLength,
+    predictionLength=predictionLength,
+    numberSamples=100,
+    dataset="fredMd",
+    collection=f"domainDatasetEmbL2_{contextLength}_{predictionLength}",
+    loadPretrainedRagCA=loadPretrainedModel,
+    fineTunedModel="MoiraiMoE-finetune-fredMd-epoch=00-step=000100.ckpt",
+    inputSpace=False,
+)
+
+report : dict = evaluation.evaluateMoiraiMoERagCA(
+    contextLength=contextLength,
+    predictionLength=predictionLength,
+    numberSamples=100,
+    dataset="electricityUCI",
+    collection=f"domainDatasetEmbL2_{contextLength}_{predictionLength}",
+    loadPretrainedRagCA=loadPretrainedModel,
+    fineTunedModel="MoiraiMoE-finetune-electricityUCI-epoch=00-step=003500.ckpt",
+    inputSpace=False,
+)
